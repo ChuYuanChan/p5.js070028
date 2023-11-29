07579eccb3733b15db0f262a0a2a1a4b92de6aa4
ChuYuanChan <starmoon1128@gmail.com>
2023-11-29 16:57:01 +0800

    4

--- final02/sketch.js
+++ final02/sketch.js
@@ -5,7 +5,7 @@ function setup() {
   
   function draw() {
     // 将背景设置为黑色，关闭填色功能。
-    background(0);
+    background('#00EC00');
     noFill();
   
     // point() 函数的两个参数分别为指定的坐标。
@@ -16,7 +16,7 @@ function setup() {
   
     // 坐标用于绘制所有形状，而不仅仅是点。
     // 不同功能的参数用于不同的目的。例如，line() 的前两个参数指定第一个端点的坐标，后两个参数指定第二个端点的坐标。
-    stroke(0, 153, 255);
+    stroke(235, 0, 0);
     line(0, height * 0.33, width, height * 0.33);
   
     // 默认情况下，rect() 的前两个参数是左上角的坐标，后两个参数是宽度和高度。
